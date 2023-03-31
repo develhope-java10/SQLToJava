937b2df8ff5edd158903298b92b93b8b12a35c38
aegroto <aegroto@protonmail.com>
2023-03-31 14:18:34 +0200

    conversion to student db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,37 @@
-import com.mysql.cj.jdbc.exceptions.ConnectionFeatureNotAvailableException;
-
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
 		Connection connection = null;
-		ArrayList<User> usersList = new ArrayList<>();
+
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum", "root", "Mysqlpass123");
-			System.out.println(connection != null);
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school", "root", "root");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 
-		Statement statement = null;
-		ResultSet resultSet = null;
-
 		try {
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM forum.users WHERE name= 'federica' AND surname = 'rossi'");
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM Students");
+
+			List<Student> students = new ArrayList<>();
+
+			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String firstName = resultSet.getString("first_name");
+				String lastName = resultSet.getString("last_name");
+
+				Student student = new Student(id, firstName, lastName);
+				students.add(student);
+			}
+
+			System.out.println(students);
 		} catch (SQLException e) {
 			System.out.println("SQLException: " + e.getMessage());
 			System.out.println("SQLState: " + e.getSQLState());
 			System.out.println("VendorError: " + e.getErrorCode());
 		}
-		try {
-			while(resultSet.next()){
-				int id = resultSet.getInt(1);
-				String name = resultSet.getString(2);
-				String surName = resultSet.getString(3);
-				User user = new User(id,name,surName);
-				usersList.add(user);
-
-			}
-		}   catch (SQLException e){
-			throw new RuntimeException(e);
-		}
-		System.out.println(usersList);
-
-
-
 	}
 }
---
+++ src/main/java/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
